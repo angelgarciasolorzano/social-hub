12bd52048cdc5380978e4184f9763fcc82abf2ec
angelgarciasolorzano <garciasolorzano19@gmail.com>
2026-04-05 11:29:30 -0600

    refactor: remove unused publication and util exportsRemove re-exports for Publication components and utility helpersfrom the public. Specifically:
    - Delete exports for PublicationCard, Publication,Form,
     and Publication from resources/js/features/publication/index.ts- Delete exports forImage andPaginatedKey from resources/js/utils/index.ts.
    
    These exports are unused and cause noisy or incorrect public modulesurfaces; removing them simplifies imports and avoidsdependencies.

--- resources/js/features/publication/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as PublicationCard } from "./components/PublicationCard";
-export { default as PublicationDetail } from "./components/PublicationDetail";
-export { default as PublicationForm } from "./components/PublicationForm";
-
-export { default as Publication } from "./Publication";
--- resources/js/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./validImage";
-export * from "./hasPaginatedKey";
