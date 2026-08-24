44d98d29a73b30e4bc1bf9981ea89abfc87c72c0
Angel Garcia <156849177+angelgarciasolorzano@users.noreply.github.com>
2026-08-24 16:33:56 -0600

    refactor: reorganize trusted device utility imports
    Move trusted device utility imports from relative path to
    absolute path for better maintainability and consistency
    across the trusted devices module.

--- resources/js/modules/setting/modules/trustedDevices/components/ui/DeviceSummaryCard.tsx
+++ resources/js/modules/setting/modules/trustedDevices/components/ui/DeviceSummaryCard.tsx
@@ -3,11 +3,13 @@ import type { JSX } from "react";
 import { FaCircle } from "react-icons/fa";
 
 import type { TrustedDevice } from "@/modules/setting/modules/trustedDevices/types/trustedDevice";
+import {
+  deviceLabel,
+  getDeviceIcon,
+} from "@/modules/setting/modules/twoFactor/utils/trustedDevice";
 
 import { iconColorVariants } from "@/shared/lib/styling";
 import { cn } from "@/shared/lib/utils";
-
-import { deviceLabel, getDeviceIcon } from "../../utils/trustedDevice";
 
 interface DeviceSummaryCardProps {
   device: TrustedDevice;
