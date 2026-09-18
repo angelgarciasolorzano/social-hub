c25ecd3b574eca7ded749bd3f9c4c61640529184
Ángel Noe García Solorzano <156849177+angelgarciasolorzano@users.noreply.github.com>
2026-09-18 15:03:04 -0600

    chore(SOC-25): enable mysql as local testing database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use RuntimeException;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Create the application for the test suite.
+     */
+    public function createApplication(): Application
+    {
+        $testingEnvironmentFile = __DIR__.'/../.env.testing';
+
+        throw_unless(is_file($testingEnvironmentFile), RuntimeException::class, 'The .env.testing file is required to run the Laravel test suite. Run composer setup first.');
+
+        return parent::createApplication();
+    }
 }
